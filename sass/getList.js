18c8890f1530e2fae5c44cdb7c38aeb26d95e93d
A M <Alcyonespace@gmail.com>
2024-03-31 10:01:44 +0300

    new homework

---
+++ sass/getList.js
@@ -0,0 +1,15 @@
+// JavaScript source code
+function buildList() {
+    var newList = document.createElement("ul");
+    var newListItem;
+    document.body.appendChild(newList);
+
+    for (var i = 1; i <= 100; i++) {
+        newListItem = document.createElement('li');
+        newListItem.textContent = i;
+        newListItem.className = 'numer' +i;
+        newList.appendChild(newListItem);
+    }
+}
+
+buildList();
