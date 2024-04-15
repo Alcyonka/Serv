b92784693142c34a21bfa2222719fca2bc5cb367
A M <Alcyonespace@gmail.com>
2024-04-15 20:58:12 +0300

    React added 1

---
+++ Advanced FakeApi Pokemon/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import PokeAPI from './PokiApi';
+
+function App() {
+  return (
+    <>
+      <PokeAPI/>
+    </>
+  );
+}
+
+export default App;
